cc6330457596ea3ac906f9125e2819e0d6aae5ea
kitahara-t <unemile@gmail.com>
2021-04-09 13:23:29 +0900

    MOD

--- aozora/src/App.js
+++ aozora/src/App.js
@@ -2,29 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import {
   BrowserRouter as Router,
+  Route,
   Link
 } from 'react-router-dom'
-import ApiFetch from './components/ApiFetch';
+
+
+import Start from './components/Start';
+import Menu from './components/Menu';
+import AozoraList from './components/AozoraList';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Router><Link to={`/components/Start`}>Go To Start</Link></Router>
-        <ApiFetch />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+         <div>
+          <Route path='/components/Start'      component={Start}/>
+          <Route path='/components/Menu'       component={Menu}/>
+          <Route path='/components/AozoraList' component={AozoraList}/>
+         </div>
+        </Router>
+        <Router>
+        <div>
+           <Link to={`/components/Start`}>Start</Link>
+           <br />
+           <br />
+        </div>
+        </Router>
     </div>
   );
 }
